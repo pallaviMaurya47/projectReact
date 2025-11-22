a88082ee1bd7e44ca844b186ce1dbb5c25bfa6a1
pallaviMaurya <kpallavi0147@gmail.com>
2025-11-22 12:46:34 +0530

    projectOngoing

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='hello'>
+      <h1>Hello1</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam repellendus, vero maiores earum inventore nobis at distinctio facere temporibus nihil incidunt impedit, rerum laborum tenetur, repellat aperiam! Veritatis, id! Cum!</p>
+    </div>
+  )
+}
+
+export default Card
